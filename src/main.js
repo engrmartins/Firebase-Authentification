4fb64bc80a6b2e29f2f12c23f04b042863118908
Enobong Martins <engrmartinsbase@gmail.com>
2019-07-18 11:45:59 +0100

    Vue App for Authentication using Firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
+import firebase from 'firebase'
+
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = ''
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDHcqc4lBRVd5G_lHYKNTs9s7Noy15Yo9s',
+  authDomain: 'user-app-1a35c.firebaseapp.com',
+  databaseURL: 'https://user-app-1a35c.firebaseio.com',
+  projectId: 'user-app-1a35c',
+  storageBucket: 'user-app-1a35c.appspot.com',
+  messagingSenderId: '25862439640',
+  appId: '1:25862439640:web:5d80d6d82ce28c84'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="login">
+        <h3>SIgn In</h3>
+        <input type="text" v-model="email" name="email" placeholder="Email"><br>
+        <input type="password" v-model="password" name="password" placeholder="Password"><br>
+        <button @click="login">Connection</button>
+        <p>You dont have an account? You can <router-link to="/sign-up"> create one</router-link></p>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        function (user) {
+          alert('Welldone ! You are now connected')
+        },
+        function (err) {
+          alert('Ooops. ' + err.message)
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+    margin-top: 40px;
+}
+input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+}
+button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+}
+p {
+    margin-top: 40px;
+    font-size: 13px;
+}
+p a {
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
